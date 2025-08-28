de623edb77958330c6d57063b7531fc59e09e4b9
Faisal Hasan Emon Bhuiyan <faisalhasanemon>
2025-08-28 18:06:52 +0600

    admin layout updated

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import SideBar from "../components/sideBar/SideBar";
+import { Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
   return (
-    <div className="font-montserrat text-5xl text-red-500">admin layout</div>
+    <main className=" w-full font-montserrat relative">
+      <section className="flex h-screen relative overflow-hidden w-full">
+        {/*1 left sidebar */}
+        <SideBar></SideBar>
+        {/* 2 right site content */}
+        <div className="w-full  px-5 overflow-y-scroll">
+          <Outlet></Outlet>
+        </div>
+      </section>
+    </main>
   );
 };
 
