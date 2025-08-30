5b225dc107d582189f7809827ed85d917d24a3a9
Faisal Hasan Emon Bhuiyan <faisalhasanemon>
2025-08-30 13:40:08 +0600

    dahboard settings page updated

---
+++ src/pages/dashboardPages/settings/personalInformation/PersonalInformation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PersonalInformation = () => {
+  return <div>This is personal information page</div>;
+};
+
+export default PersonalInformation;
---
+++ src/pages/dashboardPages/settings/trustAndSafety/TrustAndSafety.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrustAndSafety = () => {
+  return <div>Trust And Safety</div>;
+};
+
+export default TrustAndSafety;
