9dfd7fa446c39b6cd9aff98e216e3dc1883aec37
Faisal Hasan Emon Bhuiyan <faisalhasanemon>
2025-09-03 15:13:26 +0600

    In mobile and tab navbar and sidebar toggle fixed

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import { useState } from "react";
 import SideBar from "../components/sideBar/SideBar";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 
 const AdminLayout = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleLayout = () => {
+    setIsNavbarOpen((prev) => !prev);
+    setIsSidebarOpen((prev) => !prev);
+  };
+
   return (
     <main className=" w-dvw font-montserrat ">
       <section className="flex h-screen overflow-hidden w-full">
         {/*1 left sidebar */}
-        <div className="hidden sm:block">
-          <SideBar></SideBar>
+        {/* <div className="hidden sm:block"> */}
+        <div className="">
+          <SideBar isOpen={isSidebarOpen} onToggle={toggleLayout}></SideBar>
         </div>
         {/* 2 right site content */}
-        <div className="w-full px-5 overflow-y-scroll bg-[#F2F5F7] mb-5 ">
-          <Navbar></Navbar>
+        <div className="w-full  px-5 overflow-y-scroll bg-[#F2F5F7] pb-16 pt-28 lg:pt-0 ">
+          <Navbar isOpen={isNavbarOpen} onToggle={toggleLayout}></Navbar>
           <Outlet></Outlet>
         </div>
       </section>
