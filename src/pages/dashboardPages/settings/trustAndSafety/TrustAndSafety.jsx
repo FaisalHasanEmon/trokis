ba5e64f3f9ea501262a689d633c3b298bbef8c4f
Faisal Hasan Emon Bhuiyan <faisalhasanemon>
2025-09-04 12:16:05 +0600

    Trust and sfety page content updated

--- src/pages/dashboardPages/settings/trustAndSafety/TrustAndSafety.jsx
+++ src/pages/dashboardPages/settings/trustAndSafety/TrustAndSafety.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { ChevronLeft } from "lucide-react";
+import ReadAndEdit from "../../../../components/readAndEdit/ReadAndEdit";
+import { Link } from "react-router-dom";
 
 const TrustAndSafety = () => {
-  return <div>Trust And Safety</div>;
+  return (
+    <>
+      <Link
+        to="/settings"
+        className="flex justify-start items-center gap-4 mt-5"
+      >
+        <ChevronLeft />
+        <h2 className="text-2xl font-medium gap-4">Trust & Safety Edit</h2>
+      </Link>
+      <ReadAndEdit
+        title="Trust And Safety"
+        fetchUrl="/fakeAPIs/trustAndSafety.json"
+        updateUrl="/fakeAPIs/trustAndSafety.json"
+      ></ReadAndEdit>
+    </>
+  );
 };
 
 export default TrustAndSafety;
